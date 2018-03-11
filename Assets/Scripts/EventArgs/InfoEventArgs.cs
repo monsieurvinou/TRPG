5dcc5f210eb29f6852abea05aabda4f24f666e2f
Vincent Jacquottin <vjacquottin@sqli.com>
2018-03-11 16:53:06 +0100

    04 - Input Controller

---
+++ Assets/Scripts/EventArgs/InfoEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class InfoEventArgs<T> : EventArgs
+{
+    public T info;
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public InfoEventArgs()
+    {
+        this.info = default(T);
+    }
+
+    /// <summary>
+    /// Constructor with defined infos
+    /// </summary>
+    /// <param name="info"></param>
+    public InfoEventArgs(T info)
+    {
+        this.info = info;
+    }
+}
